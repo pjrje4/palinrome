280ddf0bc3ad73d09ba0bbeb7ad1d37d04cdabca
pjrje4 <58542025+pjrje4@users.noreply.github.com>
2021-09-29 13:14:45 -0700

    Add files via upload

---
+++ palindrome.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cstring>
+/*
+ *Justin Iness
+ *Guessing Game
+ *Jason Galbraith
+ *C++ Programming
+ * */
+
+using namespace std;
+
+int main() //main loop
+{
+	char stringput[80];
+	char stringput2[80];
+	char reverse[80];
+
+	cout << "Please enter a string to check if it is a palindrome:";
+
+	cin.get (stringput, 80);
+	cin.get();
+
+	int count = 0;
+	int count2 = 0;
+	int len = strlen(stringput);
+
+	for (int i=0; i<len;i++) {
+		if (isalpha(stringput[i])) {
+			stringput2[count] = tolower(stringput[i]);
+			
+			count++;
+		}		
+	}
+
+	for (int f=count-1; f>=0; f--) {
+		reverse[count2] = stringput2[f];
+		count2++;
+	}
+	if (strcmp(reverse, stringput2) == 0) {
+		cout << "is pal";
+	}
+
+
+	cout << stringput << endl;
+	cout << stringput2 << endl;
+	cout << reverse << endl;
+	cout << strlen(stringput2) << endl;
+	cout << strlen(reverse) << endl;
+
+}
